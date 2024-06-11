be859d88df58d034fb2a66e37bf446b68d90c39f
PranavK-Official <pranavkcse@gmail.com>
2024-06-11 15:42:42 +0530

    small change in bg-changer

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -25,14 +25,21 @@ function App() {
   ];
   return (
     <>
-      <div
-        className="w-screen h-screen duration-200"
-        style={{ backgroundColor: color }}
-      >
+      <div className="w-screen h-screen " style={{ backgroundColor: color }}>
         <div className="text-center p-10 bg-slate-800">
           <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
             Simple Background Changer
           </h1>
+
+          <h3 className="pt-5 text-white capitalize">
+            The color now is:{" "}
+            <span
+              className="bg-slate-400 px-2 py-1 rounded font-extrabold"
+              style={{ color: color }}
+            >
+              {color}
+            </span>
+          </h3>
         </div>
         <div className="fixed flex flex-wrap justify-center bottom-16 inset-x-0 px-2">
           <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-slate-600 px-3 py-2 rounded-xl">
