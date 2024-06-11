573668583b47e4254fa2b686484e4b559354e758
PranavK-Official <pranavkcse@gmail.com>
2024-06-11 14:38:49 +0530

    Made slight modifications for the bg-changer

---
+++ bg-changer/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("black");
+
+  const colors = [
+    {
+      color: "black",
+    },
+    {
+      color: "white",
+    },
+    {
+      color: "red",
+    },
+    {
+      color: "green",
+    },
+    {
+      color: "orange",
+    },
+    {
+      color: "purple",
+    },
+  ];
+  return (
+    <>
+      <div
+        className="w-screen h-screen duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <div className="text-center p-10 bg-slate-800">
+          <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+            Simple Background Changer
+          </h1>
+        </div>
+        <div className="fixed flex flex-wrap justify-center bottom-16 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-slate-600 px-3 py-2 rounded-xl">
+            {colors.map((colorName, index) => (
+              <button
+                key={`${colorName.color}-${index}`}
+                onClick={() => setColor(colorName.color)}
+                className="capitalize outline-none px-4 py-1 rounded-full text-white shadow-lg"
+                style={{ backgroundColor: colorName.color }}
+              >
+                {colorName.color}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
