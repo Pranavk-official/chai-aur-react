33d325f56c81b1f0ced81377f38201a06c785063
PranavK-Official <pranavkcse@gmail.com>
2024-06-16 18:21:53 +0530

    basic todo app using context and localStorage [js]

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+import { TodoProvider } from "./contexts";
+import { TodoForm, TodoItem } from "./components";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addToDo = (todo) => {
+    setTodos((currState) => [{ id: Date.now(), ...todo }, ...currState]);
+  };
+
+  const editToDo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  const deleteToDo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  // Local Storage
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <TodoProvider
+      value={{ todos, addToDo, editToDo, deleteToDo, toggleComplete }}
+    >
+      <div className="bg-zinc-900 min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-3xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  );
+}
+
+export default App;
---
+++ todo-list/src/contexts/TodoContext.js
@@ -0,0 +1,21 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "Todo Message",
+      completed: false,
+    },
+  ],
+  addToDo: (todo) => {},
+  editToDo: (id, todo) => {},
+  deleteToDo: (id) => {},
+  toggleComplete: (id) => {},
+});
+
+export const useTodo = () => {
+  return useContext(TodoContext);
+};
+
+export const TodoProvider = TodoContext.Provider;
